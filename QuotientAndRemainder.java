005190e13781d81af74e5de29687e421d58732eb
NagapriyaGanapathy <84984674+NagapriyaGanapathy@users.noreply.github.com>
2021-06-06 14:01:00 +0530

    Add files via upload

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,12 @@
+package source.code;
+
+public class QuotientAndRemainder {
+	public static void main(String[] args) {
+		int x=20,y=2;
+		int quotient=x/y;
+		int remainder=x%y;
+		System.out.println("Quotient is :"+quotient);
+		System.out.println("Remainder is :"+remainder);
+		
+	}
+}
